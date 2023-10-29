c80fbd939b43c982152d74a6d8e5971c3848cef5
Abdullaziz Hassen Abdella <abdullaziz.hassenabdella@ksb-sg.ch>
2023-10-29 22:58:37 +0100

    Geometrische Figuren

---
+++ Geometrische Figuren/Geometrische Figuren/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrische_Figuren
+{
+    internal public class Square : Rectangle
+    {
+        public Square(float x = 100.0f, float y = 100.0f, float sideLength = 10.0f)
+            : base(x, y, sideLength, sideLength)
+        {
+        }
+    }
+}

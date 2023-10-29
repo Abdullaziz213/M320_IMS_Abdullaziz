ef26df9b1f37eb0a89df3820cc60042f9730508e
Abdullaziz Hassen Abdella <abdullaziz.hassenabdella@ksb-sg.ch>
2023-10-29 23:04:13 +0100

    Geometriche Figuren

---
+++ Geometrische Figuren/Geometrische Figuren/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrische_Figuren
+{
+    internal public class Circle : Figure
+    {
+        protected float Radius { get; set; }
+
+        public Circle(float x = 100.0f, float y = 100.0f, float radius = 5.0f)
+            : base(x, y)
+        {
+            if (radius > 0)
+            {
+                Radius = radius;
+            }
+        }
+
+        public override void PrintPosition()
+        {
+            Console.WriteLine($"Position (center): [{X}, {Y}]");
+        }
+
+        public override void PrintArea()
+        {
+            double area = Math.PI * Radius * Radius;
+            Console.WriteLine($"Area: {area}");
+        }
+    }
+}
